853e333fea842bab8e638c09d8d880109c989347
DevShain <shainaktar40@gmail.com>
2021-11-28 22:28:07 +0530

    added functional behaviour in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [items, setItem] = useState([]);
+  const handle = (event)=>{
+    setInput(event.target.value);
+  }
+  const addItem = ()=>{
+    setItem((prevItem)=>{
+      return [...prevItem, input];
+    })
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input  type="text"/>
-        <button >
+        <input onChange={handle} type="text" value={input}/>
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>item</li>
+          {items.map((item)=>{
+            return <li>{item}</li>
+          })}
         </ul>
       </div>
     </div>
